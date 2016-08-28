6c481ef6d7dbe64d52ffdcae2eaa627a640c7398
Bo Gjerløw <bgjerlow@live.com>
2016-08-28 13:02:12 +0200

    Footer changes
    Added Twitter icon to footer. Also removed the atribute that previously
    had the links in the footer icons open in a new tab.

--- inc/footer.php
+++ inc/footer.php
@@ -2,8 +2,9 @@
 		</div>
 		<div class="footer">
             <div id="socialIcons">
-                <a href="https://dribbble.com/bgjerlowdesigns" target="_blank"><img src="img/icons/dribbble.png" class="icons" onmouseover="this.src='img/icons/dribbble-hover.png';" onmouseout="this.src='img/icons/dribbble.png';" alt="Dribbble"></a>
-                <a href="https://linkedin.com/in/bgjerlow" target="_blank"><img src="img/icons/linkedin.png" class="icons" onmouseover="this.src='img/icons/linkedin-hover.png';" onmouseout="this.src='img/icons/linkedin.png';" alt="LinkedIn"></a>
+                <a href="https://dribbble.com/bgjerlowdesigns"><img src="img/icons/dribbble.png" class="icons" onmouseover="this.src='img/icons/dribbble-hover.png';" onmouseout="this.src='img/icons/dribbble.png';" alt="Dribbble"></a>
+                <a href="https://linkedin.com/in/bgjerlow"><img src="img/icons/linkedin.png" class="icons" onmouseover="this.src='img/icons/linkedin-hover.png';" onmouseout="this.src='img/icons/linkedin.png';" alt="LinkedIn"></a>
+				<a href="https://twitter.com/BoGjerlow"><img src="img/icons/twitter.png" class="icons" onmouseover="this.src='img/icons/twitter-hover.png';" onmouseout="this.src='img/icons/twitter.png';" alt="Twitter"></a>
             </div>
 		</div>
 	</body>
