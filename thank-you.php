c342a18c29f176f0f5a3512a52788aaaa037c8e7
Bo Gjerløw <bgjerlow@live.com>
2016-09-03 16:52:32 +0200

    Added new sitemap.xml and fixed title on thank-you-php page

--- thank-you.php
+++ thank-you.php
@@ -3,7 +3,7 @@
 	<head>
 		<meta charset="UTF-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<title>Bo Gjerlow | Error</title>
+		<title>Bo Gjerlow | Success</title>
 		<meta name="robots" content="noindex,nofollow"/>
 		<?php include 'inc/header.php';?>
 		
