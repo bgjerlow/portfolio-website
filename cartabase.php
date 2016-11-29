aef3e6acf9075864b0707ec1b9589c62639bf4b1
Bo Gjerløw <bgjerlow@live.com>
2016-11-29 18:53:01 +0100

    Edited paragraph under 'Cartabase' portfolio piece

--- cartabase.php
+++ cartabase.php
@@ -14,8 +14,7 @@
 					<img src="img/portfolio/cartabase/cartabase-top.jpg" class="itemImage itemImageMargin" alt="Cartabase logo">
 				</section>
 				<section class="portfolioItem">
-					<p class="aboutText">Branding I did for a startup I and a friend are working on that is related to sale and purchase of used cars. It is set on hold at the moment as we are both busy with other things.</p>
-					<p class="aboutText">The colours used in the logo are the same colours we plan to use in our product.</p>
+					<p class="aboutText">Branding I did for a startup me and a friend worked on that was related to sale and purchase of used cars. This project was later shut down due to lack of time and technical abilities to develop the platform.</p>
 				</section>
 			</main>
 
