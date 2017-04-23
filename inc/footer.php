b7d3960f3336c33a443bc675cf3009b1cee08f55
Bo Gjerløw <eaabogje@students.eaaa.dk>
2017-04-23 13:23:30 +0200

    Removed Twitter link/icons from footer

--- inc/footer.php
+++ inc/footer.php
@@ -4,7 +4,6 @@
             <div id="socialIcons">
                 <a href="https://dribbble.com/bgjerlowdesigns"><img src="img/icons/dribbble.png" class="icons" onmouseover="this.src='img/icons/dribbble-hover.png';" onmouseout="this.src='img/icons/dribbble.png';" alt="Dribbble"></a>
                 <a href="https://linkedin.com/in/bgjerlow"><img src="img/icons/linkedin.png" class="icons" onmouseover="this.src='img/icons/linkedin-hover.png';" onmouseout="this.src='img/icons/linkedin.png';" alt="LinkedIn"></a>
-				<a href="https://twitter.com/BoGjerlow"><img src="img/icons/twitter.png" class="icons" onmouseover="this.src='img/icons/twitter-hover.png';" onmouseout="this.src='img/icons/twitter.png';" alt="Twitter"></a>
             </div>
 		</div>
 	</body>
