dd663a863631e7a5db66dcac06973ee6d8e94d12
Bo Gjerløw <eaabogje@students.eaaa.dk>
2017-09-19 14:26:14 +0200

    Changes to 'Work' page and more
    * Added new image overlay to the portfolio pieces on the 'Work' page.
    The title and tags for each piece will now show in the overlay
    
    * Removed all code for the previous overlay effect on 'Work'
    
    * Fixed a bug in IE where the textarea (.message) on the 'Contact' page
    would display a scrollbar
    
    * Added border-radius of 2.5px to portfolio item intro images and to the
    fields on the 'Contact' page

--- index.php
+++ index.php
@@ -15,48 +15,82 @@
 
 			<main id="homeContent" class="animated zoomIn">
                 <div class="portfolioPreview">
-					<a href="spot-festival.php" class="darken"><img src="img/portfolio/spot-festival/spot-festival-preview.jpg" class="previewImage" alt="SPOT Festival website"></a>
-					<a href="spot-festival.php"><h2 class="previewText">SPOT Festival</h2></a>
-					<h3 class="previewTags">UI design, UX design, Web development</h3>
+                    <div class="content">
+                        <a href="spot-festival.php">
+                            <div class="contentOverlay" id="spotFestivalOverlay"></div>
+                                <img src="img/portfolio/spot-festival/spot-festival-preview.jpg" class="contentImage" alt="Spot Festival">
+                                <div class="contentDetails">
+                                    <h2>SPOT Festival</h2>
+                                    <h3>UI design, UX design, Web development</h3>
+                                </div>
+                        </a>
+					</div>
 				</div>
                 
 				<div class="portfolioPreview middleMargin">
-					<a href="while-you-wait.php" class="darken"><img src="img/portfolio/while-you-wait/while-you-wait-preview.jpg" class="previewImage" alt="While You Wait logo"></a>
-					<a href="while-you-wait.php"><h2 class="previewText">While You Wait</h2></a>
-					<h3 class="previewTags">UI design, UX design, Web development, Branding</h3>
+					<div class="content">
+                        <a href="while-you-wait.php">
+                            <div class="contentOverlay" id="whileYouWaitOverlay"></div>
+                                <img src="img/portfolio/while-you-wait/while-you-wait-preview.jpg" class="contentImage" alt="Spot Festival">
+                                <div class="contentDetails">
+                                    <h2>While You Wait</h2>
+                                    <h3>UI design, UX design, Web development, Branding</h3>
+                                </div>
+                        </a>
+					</div>
 				</div>
 
 				<div class="portfolioPreview">
-					<a href="mconsult.php" class="darken"><img src="img/portfolio/mconsult/mconsult-preview.jpg" class="previewImage" alt="MConsult logo"></a>
-					<a href="mconsult.php"><h2 class="previewText">MConsult</h2></a>
-					<h3 class="previewTags">Branding</h3>
+					<div class="content">
+                        <a href="mconsult.php">
+                            <div class="contentOverlay" id="mConsultOverlay"></div>
+                                <img src="img/portfolio/mconsult/mconsult-preview.jpg" class="contentImage" alt="Spot Festival">
+                                <div class="contentDetails">
+                                    <h2>MConsult</h2>
+                                    <h3>Branding</h3>
+                                </div>
+                        </a>
+					</div>
 				</div>
 
 				<div class="portfolioPreview">
-					<a href="forza-hub-mobile.php" class="darken"><img src="img/portfolio/forza-hub-mobile/forza-hub-mobile-preview.jpg" class="previewImage" alt="Forza Hub Mobile app"></a>
-					<a href="forza-hub-mobile.php"><h2 class="previewText">Forza Hub Mobile</h2></a>
-					<h3 class="previewTags">UI design, UX design</h3>
+					<div class="content">
+                        <a href="forza-hub-mobile.php">
+                            <div class="contentOverlay" id="forzaHubMobileOverlay"></div>
+                                <img src="img/portfolio/forza-hub-mobile/forza-hub-mobile-preview.jpg" class="contentImage" alt="Spot Festival">
+                                <div class="contentDetails">
+                                    <h2>Forza Hub Mobile</h2>
+                                    <h3>UI design, UX design</h3>
+                                </div>
+                        </a>
+					</div>
 				</div>
 
 				<div class="portfolioPreview middleMargin">
-					<a href="liga-bbva-redesign.php" class="darken"><img src="img/portfolio/liga-bbva-redesign/liga-bbva-redesign-preview.jpg" class="previewImage" alt="Liga BBVA redesign"></a>
-					<a href="liga-bbva-redesign.php"><h2 class="previewText">Liga BBVA Redesign</h2></a>
-					<h3 class="previewTags">UI design</h3>
+					<div class="content">
+                        <a href="liga-bbva-redesign.php">
+                            <div class="contentOverlay" id="ligaBBVARedesignOverlay"></div>
+                                <img src="img/portfolio/liga-bbva-redesign/liga-bbva-redesign-preview.jpg" class="contentImage" alt="Spot Festival">
+                                <div class="contentDetails">
+                                    <h2>Liga BBVA Redesign</h2>
+                                    <h3>UI design</h3>
+                                </div>
+                        </a>
+					</div>
 				</div>
 
 				<div class="portfolioPreview">
-					<a href="cartabase.php" class="darken"><img src="img/portfolio/cartabase/cartabase-preview.jpg" class="previewImage" alt="Cartabase logo"></a>
-					<a href="cartabase.php"><h2 class="previewText">Cartabase</h2></a>
-					<h3 class="previewTags">Branding, UI design</h3>
+					<div class="content">
+                        <a href="cartabase.php">
+                            <div class="contentOverlay" id="cartabaseOverlay"></div>
+                                <img src="img/portfolio/cartabase/cartabase-preview.jpg" class="contentImage" alt="Spot Festival">
+                                <div class="contentDetails">
+                                    <h2>Cartabase</h2>
+                                    <h3>Branding, UI design</h3>
+                                </div>
+                        </a>
+					</div>
 				</div>
 			</main>
-
-			<script type="application/javascript">
-				$('.darken').hover(function() {
-					$(this).find('img').fadeTo(500, 0.5);
-				}, function() {
-					$(this).find('img').fadeTo(500, 1);
-				});
-			</script>
 
 			<?php include 'inc/footer.php';?>
