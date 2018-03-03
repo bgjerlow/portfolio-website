08275fb1c9f7575df7149062f11c8948ac7c499d
Bo Gjerløw <bgjerlow@live.com>
2018-03-03 17:45:28 +0100

    Updated Dribbble link in footer

--- inc/footer.php
+++ inc/footer.php
@@ -2,7 +2,7 @@
 </div>
 <footer class="footer">
     <div id="socialIcons">
-        <a href="https://dribbble.com/bgjerlowdesigns"><img src="img/icons/social-dribbble.svg" class="icons" alt="Dribbble"></a>
+        <a href="https://dribbble.com/bogjerlow"><img src="img/icons/social-dribbble.svg" class="icons" alt="Dribbble"></a>
         <a href="https://www.linkedin.com/in/bogjerlow/"><img src="img/icons/social-linkedin.svg" class="icons" alt="LinkedIn"></a>
         <a href="https://github.com/bgjerlow"><img src="img/icons/social-github.svg" class="icons" alt="GitHub"></a>
     </div>
