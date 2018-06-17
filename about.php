ce79a6cd6c0fbe579f784d8ed052eb08343419eb
Bo Gjerløw <bgjerlow@live.com>
2018-06-17 16:07:47 +0200

    Updated content on About

--- about.php
+++ about.php
@@ -12,7 +12,8 @@
 
     <main id="aboutContent" class="animated zoomIn">
         <p id="aboutIntro" class="aboutText">I'm Bo Gjerl&oslash;w, a multimedia designer based in Denmark. </br>
-        I specialise in user interface design and front-end development, but I also enjoy working with user experience design and back-end development using Ruby On Rails.</p>
+        I specialise in user interface design, front-end development &amp; communication, but I also enjoy working with user experience design and back-end development using Ruby On Rails.</br>
+        I currently work at Carswip as a Digital Brand Manager.</p>
 
         <p class="aboutText">I am passionate about creating beautiful user interfaces with a focus on the user experience. I also keep up to date with the latest trends and tendencies within UI/UX design and web development.</p>
         
