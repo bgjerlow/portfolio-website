a095a72430b8e77c127a1127697b45bb3fcab8ea
Bo Gjerløw <bgjerlow@live.com>
2018-07-22 15:14:17 +0200

    Minor CSS fix to #siteLogoMobile

--- inc/header.php
+++ inc/header.php
@@ -4,7 +4,7 @@
 <!-- Website styling -->
 <link rel="stylesheet" type="text/css" href="css/reset.css">
 <link rel="stylesheet" type="text/css" href="css/style.css?v=1.1.3">
-<link rel="stylesheet" type="text/css" href="css/media-queries.css?v=1.1.3">
+<link rel="stylesheet" type="text/css" href="css/media-queries.css?v=1.1.4">
 
 <!-- Google Fonts -->
 <link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans:400,600" rel="stylesheet">
